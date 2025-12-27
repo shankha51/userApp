d47464ecdcc82e22fd068320c2c5fc5bc678e983
Shankhadeep Das <shankha51@gmail.com>
2025-12-26 20:11:14 -0600

    file created

---
+++ src/main/java/com/shankha/userApp/UserAppApplication.java
@@ -0,0 +1,14 @@
+package com.shankha.userApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAppApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/shankha/userApp/UserAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.shankha.userApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
